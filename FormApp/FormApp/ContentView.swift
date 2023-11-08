27e6b4c77bcaa5553f5c2589d76a1499bd6bc107
Lucas Neves <lucas.neves12@icloud.com>
2023-11-08 17:38:56 -0300

    Creating the UI for the layout and button validations.

--- FormApp/FormApp/ContentView.swift
+++ FormApp/FormApp/ContentView.swift
@@ -8,14 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var name: String = ""
+    @State var email: String = ""
+    @State var feedback: String = ""
+    @State var nota: Float = 5
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    HStack {
+                        Image(systemName: "person.fill")
+                            .foregroundStyle(.gray)
+                        TextField("Nome", text: $name)
+                    }
+                    
+                    HStack {
+                        Image(systemName: "envelope.fill")
+                            .foregroundStyle(.gray)
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                    }
+                } header: {
+                    Text("Informações pessoais")
+                }
+                
+                Section {
+                    ZStack {
+                        if feedback.isEmpty { 
+                            Text("Digite seu feedback aqui")
+                                .foregroundStyle(.gray)
+                        }
+                        TextEditor(text: $feedback)
+                    }
+                    .frame(height: 60)
+                } header: {
+                    Text("Informe seu feedback")
+                }
+                
+                Section {
+                    HStack {
+                        Text("1")
+                        Slider(value: $nota, in: 1...10, step: 1)
+                        Text("10")
+                        Spacer().frame(width: 30)
+                        Text("Nota: \(Int(nota))")
+                    }
+                } header: {
+                    Text("Nota")
+                }
+                
+                Section {
+                    Button {
+                        print("tapped button")
+                    } label: {
+                        Text("Enviar Feedback")
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 44)
+                            .foregroundStyle(.white)
+                            .background(isDisabledButton ? Color.gray : Color.blue)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    }
+                    .disabled(isDisabledButton)
+                }
+            }
         }
-        .padding()
+    }
+    
+    var isDisabledButton: Bool {
+        return name.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty || email.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
     }
 }
 
