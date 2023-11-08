3e6177f8fdb4910fa042431f6622b08b8f698178
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-11-08 15:21:39 -0300

    Add files via upload

---
+++ FormApp/FormApp/FormAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormAppApp.swift
+//  FormApp
+//
+//  Created by Lucas Neves dos santos pompeu on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FormAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
